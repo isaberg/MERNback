0022cd406b0ad8a7e41721919f4538f80248f9a7
Isaiah Berg <isaiah.j.berg@gmail.com>
2018-07-05 10:40:43 -0400

    add {}

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  Quote.find()
+  Quote.find({})
     .then((quotes) => {
       res.json(quotes)
     })
